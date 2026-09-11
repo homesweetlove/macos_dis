6654f8a1cc85c5c5c0932687bac87b83d85eab75
i_so_free <zlem8952@cu.ac.kr>
2026-09-11 16:45:28 +0900

    fix(rainmeter): bundle PNG icons and remove fragile glyph dependencies

--- rainmeter/Skins/MacosDisKR/@Resources/Scripts/DateKR.lua
+++ rainmeter/Skins/MacosDisKR/@Resources/Scripts/DateKR.lua
@@ -1,8 +1,6 @@
-﻿function Initialize()
-  weekdays = {"일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"}
+function Initialize()
 end
 
 function Update()
-  local t = os.date('*t')
-  return string.format('%d월 %d일 %s', t.month, t.day, weekdays[t.wday])
+  return os.date('%A, %B %d')
 end
